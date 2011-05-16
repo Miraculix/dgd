a196bbd50705516e46805bcf62974c316b00393d
Ulrich Strauss <miraculix@xyllomer.de>
2011-05-16 17:41:42 +0200

    Merge with DGD 1.4.7

--- src/lex/token.c
+++ src/lex/token.c
@@ -367,8 +367,6 @@ static int gc()
 	    tb->inbuf--;
 	    c = UCHAR(*(tb->p)++);
 	}
-    if ( c == 0x13 )
-    continue;
 	if (c == LF && tb == ibuffer) {
 	    ibuffer->line++;
 	    if (!backslash) {
