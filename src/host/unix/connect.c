7cb169f3015931f42003ad1bf009b07ffa348efc
Ulrich Strauss <miraculix@xyllomer.de>
2012-08-16 13:43:31 +0200

    Fixed unitialized conn->addr in conn_openlisten

--- src/host/unix/connect.c
+++ src/host/unix/connect.c
@@ -1776,6 +1776,7 @@ connection *conn_openlisten(unsigned char protocol, unsigned short port)
 	conn->fd = sock;
 	conn->chain.name = (char *) NULL;
 	conn->udpbuf = (char *) NULL;
+	conn->addr = (char *) NULL;
 	sz = sizeof(sin);
 	getsockname(conn->fd, (struct sockaddr *) &sin, &sz);
 	conn->at = ntohs(sin.sin_port);
@@ -1816,6 +1817,7 @@ connection *conn_openlisten(unsigned char protocol, unsigned short port)
 	conn->fd = sock;
 	conn->chain.name = (char *) NULL;
 	conn->udpbuf = (char *) NULL;
+	conn->addr = (char *) NULL;
 	sz = sizeof(sin);
 	getsockname(conn->fd, (struct sockaddr *) &sin, &sz);
 	conn->at = ntohs(sin.sin_port);
